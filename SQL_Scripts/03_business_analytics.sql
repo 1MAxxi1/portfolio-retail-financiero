32558e2972152d369bb295e274c4b386f02bb9d5
1MAxxi1 <maximfuentes3005@gmail.com>
2026-06-28 03:42:30 -1200

    Add KPIs for debt exposure and credit utilization

--- SQL_Scripts/03_business_analytics.sql
+++ SQL_Scripts/03_business_analytics.sql
@@ -31,3 +31,33 @@ SELECT
 FROM vw_transacciones_limpias AS t
 GROUP BY t.tipo_pago
 ORDER BY suma_total DESC;
+
+-- ----------------------------------------------------------------------------------------------
+-- KPI 3: Exposición de Deuda por Nivel de Riesgo
+-- Pregunta de Negocio: ¿Cuánto capital tenemos en riesgo según el estado de nuestros deudores?
+-- ----------------------------------------------------------------------------------------------
+SELECT
+	V.estado_riesgo,
+    COUNT(V.id_cliente) AS volumen_deudores,
+    SUM(v.deuda_actual) AS deuda_total
+FROM vw_clientes_credito_limpios AS V
+GROUP BY v.estado_riesgo
+ORDER BY deuda_total DESC; 
+
+-- ----------------------------------------------------------------------------------------------
+-- KPI 4: Tasa de Utilización de Crédito
+-- Pregunta de Negocio: ¿Qué porcentaje de su límite de crédito están utilizando los clientes según su riesgo?
+-- ----------------------------------------------------------------------------------------------
+SELECT
+    v.estado_riesgo,
+    SUM(v.limite_credito) AS suma_limite_credito,
+    SUM(v.deuda_actual) AS deuda_total,
+    CONCAT(ROUND((SUM(v.deuda_actual) / NULLIF(SUM(v.limite_credito), 0)) * 100, 1), '%') AS utilizacion
+FROM vw_clientes_credito_limpios AS v
+GROUP BY v.estado_riesgo
+ORDER BY (SUM(v.deuda_actual) / NULLIF(SUM(v.limite_credito), 0)) DESC;
+
+-- ==============================================================================================
+-- BLOQUE 3: GESTIÓN DE PRODUCTOS E INVENTARIO 
+-- Objetivo: Identificar quiebres de stock y optimizar la distribución del almacén.
+-- ==============================================================================================
