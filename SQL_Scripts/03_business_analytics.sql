8064f1a89635c2ace5a68271fa8df941d8f52a0e
1MAxxi1 <maximfuentes3005@gmail.com>
2026-06-29 03:41:05 -1200

    Add KPIs for stock alerts and customer segmentation

--- SQL_Scripts/03_business_analytics.sql
+++ SQL_Scripts/03_business_analytics.sql
@@ -61,3 +61,124 @@ ORDER BY (SUM(v.deuda_actual) / NULLIF(SUM(v.limite_credito), 0)) DESC;
 -- BLOQUE 3: GESTIÓN DE PRODUCTOS E INVENTARIO 
 -- Objetivo: Identificar quiebres de stock y optimizar la distribución del almacén.
 -- ==============================================================================================
+-- ----------------------------------------------------------------------------------------------
+-- KPI 5: Alerta de Quiebre de Stock (Stock Crítico)
+-- Pregunta de Negocio: ¿Qué productos están a punto de agotarse y cuánto capital representan?
+-- ----------------------------------------------------------------------------------------------
+SELECT
+	l.nombre_local,
+    i.stock_disponible,
+    p.nombre_producto,
+    p.costo_compra,
+    (i.stock_disponible * p.costo_compra) AS capital_inmovilizado
+FROM inventario AS i 
+JOIN locales AS l ON l.id_local = i.id_local
+JOIN productos AS p ON i.id_producto = p.id_producto
+WHERE i.stock_disponible < 15
+ORDER BY i.stock_disponible ASC;
+
+-- ----------------------------------------------------------------------------------------------
+-- KPI 6: Rentabilidad y Margen por Producto
+-- Pregunta de Negocio: ¿Cuáles son nuestros productos estrella que dejan el mayor margen de utilidad?
+-- ----------------------------------------------------------------------------------------------
+SELECT
+	p.nombre_producto,
+    p.costo_compra,
+    p.precio_venta,
+    (p.precio_venta - p.costo_compra) AS ganancia_neta,
+    ROUND(((p.precio_venta - p.costo_compra) / NULLIF(p.costo_compra, 0) * 100), 1) AS margen_porcentual_ganancia
+FROM productos AS p
+ORDER BY ganancia_neta DESC;
+
+-- ==============================================================================================
+-- BLOQUE 4: COMPORTAMIENTO Y SEGMENTACIÓN DE CLIENTES
+-- Objetivo: Identificar a los consumidores de alto valor para estrategias de fidelización.
+-- ==============================================================================================
+-- ----------------------------------------------------------------------------------------------
+-- KPI 7: Ranking de Clientes VIP (Top Compradores)
+-- Pregunta de Negocio: ¿Quiénes son los 10 clientes que más ingresos generan y cuál es su frecuencia?
+-- ----------------------------------------------------------------------------------------------
+SELECT
+	c.id_cliente,
+    c.nombre_completo,
+    COUNT(t.id_transaccion) AS frecuencia_compra,
+    SUM(t.monto_total) AS volumen_gastado
+FROM vw_clientes_credito_limpios AS c 
+JOIN vw_transacciones_limpias AS t ON c.id_cliente = t.id_cliente
+GROUP BY c.id_cliente, c.nombre_completo
+ORDER BY volumen_gastado DESC
+LIMIT 10;
+
+-- ----------------------------------------------------------------------------------------------
+-- KPI 8: Segmentación de Clientes por Valor (Tiers)
+-- Pregunta de Negocio: ¿Cómo se distribuye nuestra cartera de clientes según su nivel de gasto histórico?
+-- ----------------------------------------------------------------------------------------------
+SELECT 
+    tipo_cliente,
+    COUNT(id_cliente) AS cantidad_clientes,
+    SUM(total_gastado) AS ingresos_por_segmento
+FROM (
+    SELECT 
+        c.id_cliente,
+        SUM(t.monto_total) AS total_gastado,
+        CASE 
+            WHEN SUM(t.monto_total) >= 900000 THEN 'VIP'
+            WHEN SUM(t.monto_total) >= 500000 THEN 'Regular'
+            ELSE 'Esporádico'
+        END AS tipo_cliente
+    FROM vw_clientes_credito_limpios AS c
+    JOIN vw_transacciones_limpias AS t ON c.id_cliente = t.id_cliente
+    GROUP BY c.id_cliente
+) AS subconsulta_segmentacion 
+GROUP BY tipo_cliente
+ORDER BY tipo_cliente DESC;
+
+-- ==============================================================================================
+-- BLOQUE 5: SQL AVANZADO (WINDOW FUNCTIONS Y CTEs)
+-- Objetivo: Demostrar dominio en funciones de ventana y expresiones de tabla comunes para análisis complejos.
+-- ==============================================================================================
+-- ----------------------------------------------------------------------------------------------
+-- KPI 9: Top 3 Clientes por Sucursal (Ranking Regional)
+-- Pregunta de Negocio: ¿Quiénes son los 3 clientes más valiosos en cada uno de nuestros locales?
+-- ----------------------------------------------------------------------------------------------
+WITH GastoPorClienteLocal AS(
+    SELECT
+    	l.nombre_local,
+    	c.nombre_completo,
+    	SUM(t.monto_total) AS gastado_total_cliente,
+    	DENSE_RANK() OVER (PARTITION BY l.nombre_local ORDER BY SUM(T.monto_total) DESC) AS ranking_local
+    FROM vw_clientes_credito_limpios AS c 
+    JOIN vw_transacciones_limpias AS t ON c.id_cliente = t.id_cliente
+    JOIN locales AS l ON t.id_local = l.id_local
+    GROUP BY c.nombre_completo, l.nombre_local
+)
+SELECT *
+FROM GastoPorClienteLocal
+WHERE ranking_local <= 3
+ORDER BY nombre_local ASC, ranking_local ASC;
+
+-- ----------------------------------------------------------------------------------------------
+-- KPI 10: Crecimiento Mes a Mes (Inteligencia de Tiempo)
+-- Pregunta de Negocio: ¿Cuál es la tendencia de nuestros ingresos mensuales y nuestro % de crecimiento?
+-- ----------------------------------------------------------------------------------------------
+WITH VentasMensuales AS (
+    SELECT 
+        DATE_FORMAT(fecha_venta, '%Y-%m') AS mes_venta,
+        SUM(monto_total) AS ingresos_del_mes
+    FROM vw_transacciones_limpias
+    WHERE fecha_venta IS NOT NULL 
+    GROUP BY DATE_FORMAT(fecha_venta, '%Y-%m')
+)
+SELECT 
+    mes_venta,
+    ingresos_del_mes,
+    LAG(ingresos_del_mes, 1) OVER (ORDER BY mes_venta) AS ingresos_mes_anterior,
+    ROUND(
+        ((ingresos_del_mes - LAG(ingresos_del_mes, 1) OVER (ORDER BY mes_venta)) 
+        / NULLIF(LAG(ingresos_del_mes, 1) OVER (ORDER BY mes_venta), 0)) * 100,1) AS porcentaje_crecimiento
+FROM VentasMensuales
+ORDER BY mes_venta ASC;
+
+
+
+
